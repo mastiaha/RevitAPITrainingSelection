08ecd85b10b9466833dd2420492ed317f8b29781
Yovanovich.aa <Yovanovich.aa@DESKTOP-ITANAVJ>
2022-03-28 17:45:59 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/Main.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            IList<Reference> selectedElementRefList = uidoc.Selection.PickObjects(ObjectType.Element, "Выберите Элемент");
+            var elementList = new List<Element>();
+            foreach (var selectedElement in selectedElementRefList)
+            {
+                Element element = doc.GetElement(selectedElement);
+                elementList.Add(element);
+            }
+           
+            TaskDialog.Show("Selection", $"Количество: {elementList.Count}");
+            return Result.Succeeded;
+        }
+    }
+
+}
